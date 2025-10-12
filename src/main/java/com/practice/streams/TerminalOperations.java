5de7a6036db154d94c29430fdd7f791b5c10af0a
Joban jit <jbnjeet@gmail.com>
2025-10-12 20:28:38 +0530

    termial operations

--- src/main/java/com/practice/streams/TerminalOperations.java
+++ src/main/java/com/practice/streams/TerminalOperations.java
@@ -1,6 +1,10 @@
 package com.practice.streams;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Optional;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 public class TerminalOperations {
@@ -18,13 +22,45 @@ public class TerminalOperations {
 
     public static void main(String[] args) {
 
+        // count
         long count = Stream.of("dog", "cat")
                 .count();
         System.out.println(count);
 
-        Optional<String> min = Stream.of("deer", "horse", "pig")
-                .min((s1, s2) -> s1.length() - s2.length());
-        min.ifPresent(System.out::println);
+        // Optional<T> min(Comparator)
+        // min and max takes comparator as input as they need to compare values based on something
+        Optional<String> minLength = Stream.of("deer", "horse", "pig")
+                .min(Comparator.comparingInt(String::length));
+        minLength.ifPresent(System.out::println);
+        // Optional<T> max(Comparator)
+        Comparator<Integer> maxValueComparator = Integer::compare;
+        Optional<Integer> maxValue = Stream.of(4,6,2,12,9)
+                .max(maxValueComparator);
+        maxValue.ifPresent(System.out::println);
+
+        // findAny or FindFirst are terminal operations but not reductions as they sometimes return without
+        // processing all the elements in the stream. Reductions reduce the entire stream into one value
+        //        Optional<T> findAny()
+        Optional<String> any = Stream.of("John", "Paul").findAny();
+        any.ifPresent(System.out::println);// John (Usually)
+        //        Optional<T> findFirst()
+        Optional<String> first = Stream.of("John", "Paul").findFirst();
+        first.ifPresent(System.out::println);
+
+        // anyMatch, allMatch, noneMatch
+        List<String> names = Arrays.asList("Alan", "Brian", "Colin");
+        Predicate<String> pred = name-> name.startsWith("A");
+        System.out.println(names.stream().anyMatch(pred)); // true (one does)
+        System.out.println(names.stream().allMatch(pred)); // false ( not all match, two don't)
+        System.out.println(names.stream().noneMatch(pred)); // false (one does)
+
+        // Notes: forEach is also a method in the Collection interface
+        // Streams cannot be source of for-each loop
+        // because streams do not implement the Iterable interface
+        // e.g. Stream<Integer> s = Stream.of(1);
+        // for(Integer i: s){} // error: required array or Iterable
+
+
     }
 
 }
