c9adf3def4a1d27a8bfd8a494b3e3d3f7f796664
Joban jit <jbnjeet@gmail.com>
2025-11-25 12:58:38 +0530

    some other problems

--- src/main/java/com/practice/questions/SmallProblems.java
+++ src/main/java/com/practice/questions/SmallProblems.java
@@ -1,8 +1,11 @@
 package com.practice.questions;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class SmallProblems {
@@ -10,7 +13,10 @@ public class SmallProblems {
 //        swapTwoNumbersWithoutThird();
 //        checkVowelIsPresent();
 //        checkIfListContainsOnlyOddNumber();
-        removeSpacesFromString();
+//        removeSpacesFromString();
+//        printDateInSpecificFormat();
+//        mergeTwoLists();
+        sortHashMapByValue();
 
 
     }
@@ -25,8 +31,6 @@ public class SmallProblems {
                         StringBuilder::append
                 ).toString();
         System.out.println(withoutWhiteSpaces);
-
-
     }
 
     private static void checkIfListContainsOnlyOddNumber() {
@@ -53,4 +57,51 @@ public class SmallProblems {
         a = a-b;
         System.out.println("a: "+a+", b: "+b); //a: 20, b: 10
     }
+
+    private static void printDateInSpecificFormat(){
+        String pattern1 = "MM-dd-yyyy";
+        DateTimeFormatter dateTimeFormatter1 = DateTimeFormatter.ofPattern(pattern1);
+        System.out.println(LocalDate.now().format(dateTimeFormatter1));
+
+        String pattern2 = "yyyy/MM/dd HH:mm:ss";
+        DateTimeFormatter dateTimeFormatter2 = DateTimeFormatter.ofPattern(pattern2);
+        System.out.println(LocalDateTime.now().format(dateTimeFormatter2));
+    }
+
+    private static void mergeTwoLists(){
+        List<Integer> l1 = new ArrayList<>(Arrays.asList(1, 2, 3, 5));
+        List<Integer> l2 = new ArrayList<>(Arrays.asList( 8, 13, 21, 34));
+        l1.addAll(l2);
+        System.out.println(l1);
+
+    }
+
+    private static void sortHashMapByValue(){
+        Map<String, Integer> scores = new HashMap<>();
+        scores.put("David", 95);
+        scores.put("Jane", 80);
+        scores.put("Mary", 97);
+        scores.put("Lisa", 78);
+        scores.put("Dino", 65);
+        System.out.println(scores);
+        // By sorting the values and using LinkedHashMap to preserve the insertion order
+        LinkedHashMap<String, Integer> sortedByValueLinkedHashMap = scores.entrySet()
+                .stream()
+//                .sorted((e1, e2) -> e1.getValue().compareTo(e2.getValue())) // valid
+//                .sorted(Comparator.comparing(e->e.getValue())) //valid
+//                .sorted(Comparator.comparing(Map.Entry::getValue)) // valid
+                .sorted(Map.Entry.comparingByValue()) // valid - inbuilt comparator (interface static method)
+                .collect(Collectors.toMap(
+                        e -> e.getKey(), // key mapper
+                        e -> e.getValue(), // value mapper
+                        (a, b) -> a, // merge function: if duplicate keys appears - if two keys class, keep the first one
+                        () -> new LinkedHashMap<>() // map supplier: which type of map should be created
+                ));
+
+        System.out.println(sortedByValueLinkedHashMap);
+
+
+    }
+
+
 }
--- src/main/java/com/practice/questions/StringRelated.java
+++ src/main/java/com/practice/questions/StringRelated.java
@@ -1,6 +1,7 @@
 package com.practice.questions;
 
-import java.util.Arrays;
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class StringRelated {
@@ -9,16 +10,13 @@ public class StringRelated {
 
         String str = "Hello world";
 //        reverseString(str);
-        createStringOutOfCharArray();
-
-
-
-
+//        createStringOutOfCharArray();
+        getCountOfDistinctCharactersInString();
 
 
     }
 
-    private static void createStringOutOfCharArray(){
+    private static void createStringOutOfCharArray() {
         char[] chars = {'H', 'e', 'l', 'l', 'o'};
         String s1 = new String(chars);
         System.out.println(s1);
@@ -33,14 +31,14 @@ public class StringRelated {
 
 
     private static void reverseString(String str) {
-    // using string builder - preferred
+        // using string builder - preferred
         String reversed = new StringBuilder(str).reverse().toString();
         System.out.println(reversed);
 //        // without built-in reverse()
         String result = "";
-        for(int i = str.length()-1; i>=0; i--){
+        for (int i = str.length() - 1; i >= 0; i--) {
 //            result = result + str.charAt(i);
-            result+= str.charAt(i);
+            result += str.charAt(i);
         }
 
         System.out.println(result);
@@ -72,6 +70,62 @@ public class StringRelated {
                 .reverse()
                 .toString();
         System.out.println(usingStringChars);
+
+    }
+
+
+    private static void getCountOfDistinctCharactersInString() {
+        String str1 = "abcdABCDabcd";
+        LinkedHashMap<Character, Long> charCounts = str1.chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(
+                        c -> c, //this groups equal characters together.
+                        LinkedHashMap::new, //map supplier: Store the grouping result inside a LinkedHashMap(preserves insertion order).
+                        Collectors.counting() // This tells Java what to do with each group of characters: Count how many items are in each group
+                ));
+        System.out.println(charCounts);
+
+        // case-insensitive distinct character count
+        TreeMap<Character, Long> charCountsIgnoreCase = str1.toLowerCase().chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(
+                        c -> c, //this groups equal characters together.
+                        TreeMap::new, //map supplier: Store the grouping result inside a TreeMap(use natural order: a->z, A-Z, 0-9). // by default it is HashMap
+                        Collectors.counting() // This tells Java what to do with each group of characters: Count how many items are in each group
+                ));
+        System.out.println(charCountsIgnoreCase);
+
+        // sorted by character frequency (ascending)
+        // first create a map then sort the map using values(character count)
+        Map<Character, Long> sortedMapWithCharacterFrequency = str1.chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(
+                        c -> c, //this groups equal characters together.
+                        Collectors.counting() // This tells Java what to do with each group of characters: Count how many items are in each group
+                ))
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue())
+                .collect(Collectors.toMap(
+                        e -> e.getKey(),
+                        e -> e.getValue(),
+                        (a, b) -> a,
+                        () -> new LinkedHashMap<>()
+                ));
+        System.out.println(sortedMapWithCharacterFrequency);
+
+        // regular loop (fastest) using merge function
+        //merge(K key, V value, BiFunction<V,V,V> remappingFunction)
+        // This means:
+        //If the key does NOT exist in the map, insert it with the given value.
+        //If the key already exists, combine the old and new value using the given remappingFunction.
+        LinkedHashMap<Character, Integer> counts = new LinkedHashMap<>();
+        for(char ch: str1.toCharArray()){
+            counts.merge(ch, 1, (v1,v2)->v1+v2);
+            // remember for same key initial values would always be 1 (what we have defined)
+            // so v2 will always be 1
+        }
+        System.out.println(counts);
 
     }
 
